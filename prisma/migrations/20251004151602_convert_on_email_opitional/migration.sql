07513eea79ff3bdae74a1a3211e6cae583f104a2
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-10-04 12:43:04 -0300

    fix: corrigindo schema do prisma e rodando migrate dev

---
+++ prisma/migrations/20251004151602_convert_on_email_opitional/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Customer" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "phone" TEXT,
+    "businessId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Customer_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Customer" ("businessId", "createdAt", "email", "id", "name", "phone", "updatedAt") SELECT "businessId", "createdAt", "email", "id", "name", "phone", "updatedAt" FROM "Customer";
+DROP TABLE "Customer";
+ALTER TABLE "new_Customer" RENAME TO "Customer";
+CREATE UNIQUE INDEX "Customer_email_businessId_key" ON "Customer"("email", "businessId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
